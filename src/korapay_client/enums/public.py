608a83c3425939818e7d4b5ae24ccfb02ea37329
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-03-31 06:15:53 +0100

    Add MobileMoneyOperator enum

--- src/korapay_client/enums/public.py
+++ src/korapay_client/enums/public.py
@@ -1,6 +1,13 @@
 from enum import Enum
 
 
+class MobileMoneyOperator(str, Enum):
+    SAFARICOM_KENYA = "safaricom-ke"
+    AIRTEL_KENYA = "airtel-ke"
+    AIRTEL_GHANA = "airtel-gh"
+    MTN_GHANA = "mtn-gh"
+
+
 class Currency(str, Enum):
     NGN = "NGN"
     KES = "KES"
