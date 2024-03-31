602517152a3a749fc3314124eb5f40a9cfb1cfbc
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-03-31 06:15:20 +0100

    Implement more method parameter validators

--- src/korapay_client/models/internal.py
+++ src/korapay_client/models/internal.py
@@ -6,13 +6,12 @@ from pydantic import (
     EmailStr,
     field_validator,
     model_serializer,
-    HttpUrl,
     SerializerFunctionWrapHandler,
     field_serializer,
+    HttpUrl,
 )
 
-from korapay_client.enums.public import Currency, PaymentChannel
-from korapay_client.models.public import Card
+from korapay_client.enums.public import Currency, PaymentChannel, MobileMoneyOperator
 
 MAX_METADATA_FIELDS = 5
 MAX_METADATA_FIELD_KEY_CHAR = 20
@@ -45,27 +44,33 @@ class MetadataValidationMixin:
         return value
 
 
-class ChargeViaCardModel(MetadataValidationMixin, BaseModel):
+class Card(BaseModel):
+    number: str
+    cvv: str
+    expiry_month: str
+    expiry_year: str
+    name: str | None = None
+    pin: str | None = None
+
+
+class ChargeViaCardModel(SerializeAmountMixin, MetadataValidationMixin, BaseModel):
     reference: str
     customer_name: str
     customer_email: EmailStr
     card: Card
-    amount: int
+    amount: int | float | Decimal
     currency: Currency
     redirect_url: HttpUrl | None = None
     metadata: dict[str, Any] | None = None
 
-    @model_serializer
-    def serialize_self(self) -> dict[str, Any]:
-        return {
-            "reference": self.reference,
-            "card": self.card,
-            "amount": self.amount,
-            "currency": self.currency,
-            "customer": {"name": self.customer_name, "email": self.customer_email},
-            "redirect_url": self.redirect_url,
-            "meta": self.metadata,
-        }
+    @model_serializer(mode="wrap")
+    def serialize_self(self, handler: SerializerFunctionWrapHandler) -> dict[str, Any]:
+        data = handler(self)
+        customer_name = data.pop("customer_name")
+        customer_email = data.pop("customer_email")
+        data["redirect_url"] = str(data["redirect_url"])
+        data["customer"] = {"name": customer_name, "email": customer_email}
+        return data
 
 
 class ChargeViaBankTransferModel(MetadataValidationMixin, BaseModel):
@@ -164,6 +169,77 @@ class InitiateChargeModel(SerializeAmountMixin, BaseModel):
         customer_email = data.pop("customer_email")
         customer_name = data.pop("customer_name", None)
         data["customer"] = {"email": customer_email}
+        if customer_name:
+            data["customer"]["name"] = customer_name
+        return data
+
+
+class PayoutToBankAccountModel(SerializeAmountMixin, BaseModel):
+    reference: str
+    amount: int | float | Decimal
+    currency: Currency
+    bank_code: str
+    account_number: str
+    customer_email: str
+    narration: str | None = None
+    customer_name: str | None = None
+
+    @model_serializer(mode="wrap")
+    def serialize_model(self, handler: SerializerFunctionWrapHandler) -> dict[str, Any]:
+        data = handler(self)
+        amount = data.pop("amount")
+        currency = data.pop("currency")
+        bank_code = data.pop("bank_code")
+        account_number = data.pop("account_number")
+        customer_email = data.pop("customer_email")
+        narration = data.pop("narration", None)
+        customer_name = data.pop("customer_name", None)
+        data["destination"] = {
+            "type": "bank_account",
+            "amount": amount,
+            "currency": currency,
+            "bank_account": {"bank": bank_code, "account": account_number},
+            "customer": {"email": customer_email},
+        }
+        if narration:
+            data["destination"]["narration"] = narration
+        if customer_name:
+            data["destination"]["customer"]["name"] = customer_name
+        return data
+
+
+class PayoutToMobileMoneyModel(SerializeAmountMixin, BaseModel):
+    reference: str
+    amount: int | float | Decimal
+    currency: Currency
+    mobile_money_operator: MobileMoneyOperator | str
+    mobile_number: str
+    customer_email: str
+    customer_name: str | None = None
+    narration: str | None = None
+
+    @model_serializer(mode="wrap")
+    def serialize_model(self, handler: SerializerFunctionWrapHandler) -> dict[str, Any]:
+        data = handler(self)
+        amount = data.pop("amount")
+        currency = data.pop("currency")
+        mobile_money_operator = data.pop("mobile_money_operator")
+        mobile_number = data.pop("mobile_number")
+        customer_email = data.pop("customer_email")
+        customer_name = data.pop("customer_name", None)
+        narration = data.pop("narration", None)
+        data["destination"] = {
+            "type": "mobile_money",
+            "amount": amount,
+            "currency": currency,
+            "mobile_money": {
+                "operator": mobile_money_operator,
+                "mobile_number": mobile_number,
+            },
+            "customer": {"email": customer_email},
+        }
+        if narration:
+            data["destination"]["narration"] = narration
         if customer_name:
             data["customer"]["name"] = customer_name
         return data
