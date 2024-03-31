8595b2ace051db42c75f703c578f3bd9acf51b95
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-03-31 09:59:56 +0100

    Rename AbstractBaseClientTestCase to AbstractClientTestCase

--- tests/test_clients/base_client_testcase.py
+++ tests/test_clients/base_client_testcase.py
@@ -1,3 +1,5 @@
+import random
+
 from dotenv import load_dotenv
 from abc import ABC, abstractmethod
 from typing import Type
@@ -5,7 +7,7 @@ from typing import Type
 from korapay_client.base_clients import AbstractBaseClient
 
 
-class AbstractBaseClientTestCase(ABC):
+class AbstractClientTestCase(ABC):
     @classmethod
     def setUpClass(cls):
         client_class = cls.get_client_class()
@@ -24,3 +26,9 @@ class AbstractBaseClientTestCase(ABC):
     @classmethod
     @abstractmethod
     def get_client_class(cls) -> Type[AbstractBaseClient]: ...
+
+    def _generate_number(self, length: int) -> int:
+        return random.randint(10 ** (length - 1), 10**length - 1)
+
+    def _generate_bvn(self) -> str:
+        return str(self._generate_number(11))
